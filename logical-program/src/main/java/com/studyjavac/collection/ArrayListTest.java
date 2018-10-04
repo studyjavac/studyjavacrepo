a05284abac0315f2a084a7047b1a12b71cabc853
skssushilk <skssushilk@gmail.com>
2018-10-05 01:02:08 +0530

    adding some

--- logical-program/src/main/java/com/studyjavac/collection/ArrayListTest.java
+++ logical-program/src/main/java/com/studyjavac/collection/ArrayListTest.java
@@ -3,8 +3,8 @@ package com.studyjavac.collection;
 public class ArrayListTest {
 	
 	public static void main(String[] args) {
-		System.out.println("Test");
 		System.out.println("good night");
+		
 	}
 
 }
